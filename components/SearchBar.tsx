f84e214b2e4b2b49c6ca8e672b197ff0d9b84a6a
Florence Martin <florence.martin469@gmail.com>
2025-01-21 19:20:29 +0100

    style: searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -10,7 +10,7 @@ export default function SearchBar({ value, onChange }: SearchBarProps) {
     <div className="relative">
       <input
         type="text"
-        placeholder="Search components..."
+        placeholder="Search for components or a category..."
         value={value}
         onChange={(e) => onChange(e.target.value)}
         className="w-full px-4 py-2 pl-10 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
